43e3f3207001bab232d228a6f8958039b36e3fd0
AmoghParulekar2003 <amoghmparulekar@gmail.com>
2024-06-16 12:45:49 +0530

    upside down column numbered right angled triangle

---
+++ 1.2_Patterns/6.cpp
@@ -0,0 +1,21 @@
+/*
+1   2   3   4   5
+1   2   3   4
+1   2   3
+1   2
+1
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int rows = 5;
+    for(int i = rows; i > 0; i--) {
+        for(int j = 1; j <= i; j++) {
+            cout<<j<<"\t";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
